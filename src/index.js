1ad9dd2abcc0d642242d96f51f9da06f4f425681
Ridwan <ridwanediallo99@gmail.com>
2022-07-13 21:37:49 +0000

    Remove index.css file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import './index.css';
 import App from './App';
 import store from './redux/bookStore';
 
